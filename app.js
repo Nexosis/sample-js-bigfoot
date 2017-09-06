04ab2b47a6528ee891c9fea24275500d62542e99
Guy Royse <guy@guyroyse.com>
2017-09-06 11:24:29 -0400

    more parallel

--- app.js
+++ app.js
@@ -8,18 +8,31 @@ const session = require('./src/session');
 const results = require('./src/results');
 const saver = require('./src/saver');
 
-loader.loadToNexosis(client, 'bigfootsightings', 'bfro-report-locations.csv')
-  .then(() => session.createForecast(client, 'bigfootsightings', '2017-01', '2020-01'))
-  .then(sessionId => results.fetch(client, sessionId))
-  .then(results => Promise.all([
-    saver.saveDataToCSV(results, 'output/bigfoot-sightings-forecast.csv'),
-    saver.saveToJSON(results, 'output/bigfoot-sightings-forecast.json')
-  ]))
-  .then(() => session.createImpactAnalysis(client, 'bigfootsightings', '1994-01', '2002-01', 'x-files'))
-  .then(sessionId => results.fetch(client, sessionId))
-  .then(results => Promise.all([
-    saver.saveDataToCSV(results, 'output/x-files-impact-on-bigfoot-sightings.csv'),
-    saver.saveMetricsToCSV(results, 'output/x-files-impact-on-bigfoot-sightings-metrics.csv'),
-    saver.saveToJSON(results, 'output/x-files-impact-on-bigfoot-sightings.json')
+loader
+  .loadToNexosis(client, 'bigfootsightings', 'bfro-report-locations.csv')
+  .then(() => Promise.all([
+    processForecstSession(),
+    processImpactSession()
   ]))
   .catch(reason => console.log('Error:', reason));
+
+function processForecstSession() {
+  return session
+    .createForecast(client, 'bigfootsightings', '2017-01', '2020-01')
+    .then(sessionId => results.fetch(client, sessionId))
+    .then(results => Promise.all([
+      saver.saveDataToCSV(results, 'output/bigfoot-sightings-forecast.csv'),
+      saver.saveToJSON(results, 'output/bigfoot-sightings-forecast.json')
+    ]))
+}
+
+function processImpactSession() {
+  return session
+    .createImpactAnalysis(client, 'bigfootsightings', '1994-01', '2002-01', 'x-files')
+    .then(sessionId => results.fetch(client, sessionId))
+    .then(results => Promise.all([
+      saver.saveDataToCSV(results, 'output/x-files-impact-on-bigfoot-sightings.csv'),
+      saver.saveMetricsToCSV(results, 'output/x-files-impact-on-bigfoot-sightings-metrics.csv'),
+      saver.saveToJSON(results, 'output/x-files-impact-on-bigfoot-sightings.json')
+    ]))
+}
